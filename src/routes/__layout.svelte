309b3bc3126c0892ee58b91f89e8585a035a6db6
enfin20 <96887337+enfin20@users.noreply.github.com>
2025-01-09 08:20:46 +0100

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,22 +2,14 @@
 </script>
 
 <nav>
-  <div
-    class="grid grid-cols-5 md:grid-cols-6 text-xs md:text-base bg-sky-700 px-2 py-2 "
-  >
+  <div class="grid grid-cols-5 md:grid-cols-5 text-xs md:text-base bg-sky-700 px-2 py-2 ">
     <div>
-      <a
-        href="/"
-        class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-      >
+      <a href="/" class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75">
         Dashboard</a
       >
     </div>
     <div>
-      <a
-        href="/opfd"
-        class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-      >
+      <a href="/opfd" class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75">
         Opfd
       </a>
     </div>
@@ -30,14 +22,11 @@
       </a>
     </div>
     <div>
-      <a
-        href="/cash"
-        class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
-      >
+      <a href="/cash" class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75">
         Tréso
       </a>
     </div>
-    <div>
+    <!--    <div>
       <a
         href="/annualexpenses"
         class=" px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75"
@@ -45,6 +34,7 @@
         Annuelles
       </a>
     </div>
+    -->
   </div>
 </nav>
 <div class="px-2 py-2 w-full">
