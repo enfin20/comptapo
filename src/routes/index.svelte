b73e328a16fe833c4184d996867d6fdd9d7b4861
enfin20 <96887337+enfin20@users.noreply.github.com>
2025-01-11 12:16:17 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -389,7 +389,7 @@
             meta.data.forEach((bar) => {
               const { x, y, base, width } = bar;
               // Réduction de la largeur du fond
-              const reduction = Math.min(width * 0.4, 5); // Réduction en pixels
+              const reduction = Math.max(width * 0.4, 15); // Réduction en pixels
               const innerWidth = width - reduction;
               let innerHeight = Math.max(0, base - y - reduction / 2);
 
